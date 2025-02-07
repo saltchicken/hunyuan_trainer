fa89c384f106b9ae0c2610318e58e5190fd7e8e1
saltchicken <john.eicher89@gmail.com>
2025-02-06 19:53:12 -0800

    Update pyproject.toml

--- src/hunyuan_trainer/__main__.py
+++ src/hunyuan_trainer/__main__.py
@@ -11,7 +11,7 @@ from pathlib import Path
 import platform
 import shutil
 
-script_dir = Path(__file__).parent / "src" / "hunyuan_trainer"
+script_dir = Path(__file__).parent
 
 def setup_training_folder(target_folder):
     training_folder = Path.home() / "hunyuan_training"
